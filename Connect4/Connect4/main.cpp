3fa196af6464f52c99510e2b3646b68c967059e0
AlexisCoteRemy <alcotre@outlook.com>
2026-04-14 10:03:31 -0400

    wtf

--- Connect4/Connect4/main.cpp
+++ Connect4/Connect4/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include "game.h"
@@ -6,6 +5,3 @@
 int main() {
 	return 0;
 }
-=======
-// All
->>>>>>> 7b09ef00f4392f204dd1f5ea22fab0db07ae76db
