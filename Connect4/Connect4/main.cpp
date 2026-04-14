f7277f3677b70495cfb64fba7a02c266e67ae0d9
AlexisCoteRemy <alcotre@outlook.com>
2026-04-14 10:12:39 -0400

    RenderWindow

--- Connect4/Connect4/main.cpp
+++ Connect4/Connect4/main.cpp
@@ -2,6 +2,27 @@
 #include <SFML/Audio.hpp>
 #include "game.h"
 
+using namespace sf;
+
 int main() {
+
+	RenderWindow window;
+
+	window.create(VideoMode(800, 608), "Connect4");
+	window.setFramerateLimit(60);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear(Color::Black);
+		window.display();
+	}
+
 	return 0;
 }
