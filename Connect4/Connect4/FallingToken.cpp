b61b10c3298171dc33409f9d9e5b96fe24d8d018
CharlesEtienneCouture <206286683@cegepsherbrooke.qc.ca>
2026-04-30 10:23:26 -0400

    ajout classe

---
+++ Connect4/Connect4/FallingToken.cpp
@@ -0,0 +1,3 @@
+#include "FallingToken.h"
+
+
---
+++ Connect4/Connect4/FallingToken.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "Button.h"
+#include "State.h"
+#include "mesConstantes.h"
+class FallingToken
+{
+private:
+	bool _falling;
+	bool _stopped;
+
+	int _col;
+	int _row;
+	int _player;
+
+	float _y;
+	float _targetY;
+	float _speed;
+
+public:
+	bool isFalling() const;
+	bool isStopped() const;
+
+	int getCol() const;
+	int getRow() const;
+	int getPlayer() const;
+
+	void start(int col, int row, int player);
+	void update(float deltaTime);
+	void draw(RenderWindow& window);
+};
+
